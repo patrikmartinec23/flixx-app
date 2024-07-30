218ad1a2d01d77db93612c16ab213de8f48d8b20
Patrik Martinec <patrik.martinec1@gmail.com>
2024-07-31 00:00:53 +0200

    Added afirst simple router

--- js/script.js
+++ js/script.js
@@ -2,13 +2,38 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
 // Init App
 function init() {
   switch (global.currentPage) {
     case '/':
+    case '/index.html':
       console.log('Home');
       break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
   }
+
+  highlightActiveLink();
 }
 
-console.log(global.currentPage);
+document.addEventListener('DOMContentLoaded', init);
